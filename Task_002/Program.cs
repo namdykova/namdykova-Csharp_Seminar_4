c2857044272730b767642ed3738699b520ff7029
Irina <namdykova03@gmail.com>
2024-02-22 15:53:02 +0300

    add Task_002

---
+++ Task_002/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] newArray = GenerateArray(5, 100, 999);
+PrintArray(newArray);
+Console.WriteLine("Кличество четных чисел: " + CountEven(newArray));
+
+int[] GenerateArray (int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int CountEven (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
